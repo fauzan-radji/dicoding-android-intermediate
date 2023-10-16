4d564d74103b3655961a93d23257d91f630e0ceb
Fauzan Radji <triputrafauzan@gmail.com>
2023-10-16 21:16:42 +0800

    Advanced UI: Latihan Membuat Custom Button dan EditText

---
+++ MyCustomView/app/src/main/java/com/fauzan/mycustomview/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.fauzan.mycustomview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myButton = findViewById(R.id.my_button)
+        myEditText = findViewById(R.id.my_edit_text)
+
+        setMyButtonEnable()
+
+        myEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // Do nothing
+            }
+        })
+
+        myButton.setOnClickListener { Toast.makeText(this@MainActivity, myEditText.text, Toast.LENGTH_SHORT).show() }
+    }
+
+    private fun setMyButtonEnable() {
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
+    }
+}
