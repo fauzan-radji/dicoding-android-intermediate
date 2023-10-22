a8eb148e7b2e1394c4900f311b5a3649c38151b8
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2023-10-23 00:35:07 +0800

    Media: Latihan Mengunggah File Ke Server dengan Multipart dalam Retrofit

--- LatihanCameraStarter/app/build.gradle.kts
+++ LatihanCameraStarter/app/build.gradle.kts
@@ -40,6 +40,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.exifinterface:exifinterface:1.3.6")
     val cameraxVersion = "1.3.0"
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
