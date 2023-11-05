de2655bd508d5c9041f59263140b1fb1fae789d7
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2023-11-06 05:37:37 +0800

    Advanced Database: Latihan Relasi Database Room

---
+++ MyStudentData/app/src/main/java/com/dicoding/mystudentdata/adapter/StudentWithCourseAdapter.kt
@@ -0,0 +1,48 @@
+package com.dicoding.mystudentdata.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.mystudentdata.database.StudentWithCourses
+import com.dicoding.mystudentdata.databinding.ItemStudentBinding
+
+class StudentWithCourseAdapter :
+    ListAdapter<StudentWithCourses, StudentWithCourseAdapter.WordViewHolder>(WordsComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val binding = ItemStudentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return WordViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class WordViewHolder(private val binding: ItemStudentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: StudentWithCourses) {
+            binding.tvItemUniversity.text = data.studentAndUniversity.university?.name
+            binding.tvItemUniversity.visibility = View.VISIBLE
+            binding.tvItemName.text = data.studentAndUniversity.student.name
+            val arrayCourse = arrayListOf<String>()
+            data.courses.forEach { course ->
+                arrayCourse.add(course.name)
+            }
+            binding.tvItemCourse.text = arrayCourse.joinToString(", ")
+            binding.tvItemCourse.visibility = View.VISIBLE
+        }
+    }
+
+    class WordsComparator : DiffUtil.ItemCallback<StudentWithCourses>() {
+        override fun areItemsTheSame(oldItem: StudentWithCourses, newItem: StudentWithCourses): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: StudentWithCourses, newItem: StudentWithCourses): Boolean {
+            return oldItem.studentAndUniversity.student.name == newItem.studentAndUniversity.student.name
+        }
+    }
+}
